8cc9c7382311bd8e97dc9439e61aa4f2c6909a60
Guilherme Alima <murakami.guilherme08@gmail.com>
2024-07-12 13:24:44 -0300

    mensagens atualizadas no sistema

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ import { turboLogger } from "./turboLogger.js";
 
 console.log("Estou usando o GitHub");
 turboLogger("estou aprendendo o fluxo do git");
+turboLogger("new message without afect the main branch", "darkgreen");
